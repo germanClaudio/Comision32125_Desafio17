5edb35d3455cd18b2a865d2ee2132879143dcbf0
German Montalbetti <germontalbetti@hotmail.com>
2023-02-01 20:15:24 -0300

    [UPDATE] Actualizacion de acuerdo arquitertura_persistencia Martin - Coder - 1-2-2032

--- api/getAllProducts.js
+++ api/getAllProducts.js
@@ -2,7 +2,8 @@ const productRepository = require('../repository/repository')
 
 const getAllProducts = async (req, res) => {
     try {
-        const products = await productRepository.getAllProducts();
+        const products = await productRepository.getAllProducts()
+        console.log('products: ', products)
         res.status(200).json({
             status: true,
             data: products,
